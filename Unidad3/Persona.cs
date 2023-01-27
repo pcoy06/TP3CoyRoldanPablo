7289d4429640e7e48c4cd774e55333e74616c785
Pablo Coy <pcoy06@gmail.com>
2023-01-26 22:51:40 -0300

    Agregar archivos de proyecto.

---
+++ Unidad3/Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad3
+{
+    class Persona
+   {
+        //declaramos los atributos
+        private string nombre;
+        private string email;
+        private int sueldo;
+        private string sexo;
+        private int peso;
+        private double altura;
+
+        // declaramos el constructor
+        public Persona(string nombre, int sueldo, string sexo, int peso, double altura)
+        {
+            this.nombre = nombre;
+            this.sueldo = sueldo;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+            this.email = CrearEmail();
+                         
+        }
+        
+        
+
+        // Método para concatenar el nombre que carguen,con el correo
+        private string CrearEmail()
+        {
+            return nombre + "@superempresa.com";
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+        public int getSueldo()
+        {
+            return sueldo;
+        }
+        
+        public string getSexo()
+        {
+            return sexo;
+        }
+        public int getPeso()
+        {
+            return peso;
+        }
+        public double getAltura()
+        {
+            return altura;
+        }              
+
+    }
+}
---
+++ Unidad3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            //string email;
+            int sueldo;
+            string sexo;
+            int peso;
+            double altura;
+
+            Console.WriteLine("-----------Bienvenido al Registro de Personas:---------- ");
+            Console.WriteLine("");
+            Console.Write("Por favor ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("De cuanto es su sueldo?: ");
+            sueldo = int.Parse(Console.ReadLine());
+            Console.Write("¿Cual es su sexo?: ");
+            sexo = Console.ReadLine();
+            Console.Write("¿Cual es su Peso actual?: ");
+            peso = int.Parse(Console.ReadLine());
+            Console.Write("¿Cual es su altura?: ");
+            altura = double.Parse(Console.ReadLine());
+
+            Persona persona = new Persona(nombre, sueldo, sexo, peso, altura);
+            
+            
+            Console.WriteLine("");
+            Console.WriteLine("** Muchas gracias, estos serían sus datos cargados: ");
+            Console.WriteLine("");
+            Console.WriteLine("Nombre: " + persona.getNombre() + "\nEmail: " + persona.getEmail() + "\nSueldo: " + persona.getSueldo() + " ARS" +  
+                "\nSexo: " + persona.getSexo() + "\nPeso: " + persona.getPeso() + " Kg" + "\nAltura: " + persona.getAltura() + " Mts.");
+
+            Console.ReadKey();
+
+        }
+        
+
+
+
+
+    }
+    
+        
+
+}
+
+
